5f7778463878429a3bd85b87978eebff8e600cdb
Leandro <leandromacedo12@gmail.com>
2022-09-09 22:23:36 -0300

    feat: Create actions to BookController

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,39 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    render json: {'ok': 'ok'}
+    books = Book.all
 
+    render json: books
+  end
+
+  def create
+    book = Book.create(permit_params)
+
+    render json: book
+  end
+
+  def show
+    book = Book.find(params[:id])
+
+    render json: book
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(permit_params)
+
+    book_updated = Book.find(params[:id])
+
+    render json: book_updated
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+  end
+
+  private
+
+  def permit_params
+    params.require(:book).permit(:title, :description, :genre, :author, :publication_at, :publisher)
   end
 end
